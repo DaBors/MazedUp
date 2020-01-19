535a8133589b1a87a428cd0e4706c23b008ad29c
enok <stefan.enoksson@gmail.com>
2020-01-19 10:45:24 +0100

    Added camera pre-follow ??? to X

--- MazedUpGame/Assets/Scripts/CameraSmoothFollow.cs
+++ MazedUpGame/Assets/Scripts/CameraSmoothFollow.cs
@@ -6,6 +6,7 @@ public class CameraSmoothFollow : MonoBehaviour
     public float FollowSpeed = 10f;
     public float FollowY = 10f;
     public float FollowZ = 2.5f;
+    public float FollowX = 2.5f;
 
     Vector3 newPos;
     float h;
@@ -21,7 +22,7 @@ public class CameraSmoothFollow : MonoBehaviour
     {
         h = Input.GetAxis("Horizontal");
 
-        newPos.x = Target.position.x;
+        newPos.x = Target.position.x + (FollowX * gameObject.transform.parent.GetComponentInChildren<PlayerController>().hSpeed);
         newPos.y = Target.position.y + FollowY;
         newPos.z = Target.position.z + (FollowZ * gameObject.transform.parent.GetComponentInChildren<PlayerController>().vSpeed);
 
